251cde06e8d317f8df081c57b30bd39c039c2075
Wilson Padilla <wpadilla4@cuc.edu.co>
2020-04-27 18:39:44 -0500

    cambios en el agregar alquiler

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { message, List } from 'antd';
+import { message , List} from 'antd';
 import { getClients } from './js/ClientFuncions'
 import { getClothWithOutRental, createRental } from './js/RentalFunctions'
 
@@ -15,7 +15,8 @@ export default class Rental extends React.Component {
             clients: [],
             client_id: '',
             rentals: [],
-            editDisable: false
+            editDisable: false,
+            isChecked : ""
 
         }
         this.onSubmit = this.onSubmit.bind(this)
@@ -42,20 +43,14 @@ export default class Rental extends React.Component {
         })
     }
 
-
-
     onChange = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
         })
     }
 
     onSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state.date_return)
-        console.log(this.state.price)
-        console.log(this.state.cloth_id)
-        console.log(this.state.client_id)
         message.loading('Registro en Proceso..', 2.5)
             .then(
                 createRental(
@@ -104,30 +99,65 @@ export default class Rental extends React.Component {
                                     </div>
                                 </div>
                                 <div className='form-group'>
-                                    <div className='mb-2'>
+                                    <div className='mb-2 mt-2'>
                                         <label>Prendas</label>
-                                    </div>
-                                    <div>
-                                        <select className="custom-select" required id='cloth_id' name='cloth_id'
-                                            value={this.state.cloth_id || ''}
-                                            onChange={this.onChange.bind(this)}
-                                            required>
-                                            <option selected disabled value="">Elige...</option>
-                                            {this.state.cloths.map((cloth, index) => (
-                                                <option key={index} value={cloth.id}>
-                                                    {cloth.name} - {cloth.color} - {cloth.fashion}
-                                                </option>
-                                            ))}
-                                        </select>
+                                        <List
+                                            grid={{
+                                                gutter: 16,
+                                                xs: 2,
+                                                sm: 3,
+                                                md: 3,
+                                                lg: 3,
+                                            }}
 
+                                            pagination={{
+                                                onChange: page => {
+                                                    console.log(page);
+                                                },
+                                                pageSize: 3,
+                                            }}
+
+                                            dataSource={this.state.cloths}
+                                            renderItem={cloth => (
+                                                <List.Item>
+                                                    <div className='card'>
+                                                        <div className='row no-gutters'>
+                                                            <div className="col-md-5 p-2 d-flex align-items-center justify-content-center">
+                                                                <img src={cloth.image} className="card-img-top" alt="moda de referencia" />
+                                                            </div>
+                                                            <div className="col-md-7">
+                                                                <div className='card-body'>
+                                                                <input type="radio" 
+                                                                    id={cloth.id} 
+                                                                    name="cloth_id" 
+                                                                    className="custom-control-input"
+                                                                    value={cloth.id} 
+                                                                    onChange={this.onChange.bind(this)}
+                                                                    
+                                                                    />
+                                                                <label className="custom-control-label" htmlFor={cloth.id}>
+                                                                    <h5 className="card-title text-center"> Prenda : {cloth.name}</h5>
+                                                                </label>
+                                                                    <ul className="list-group">
+                                                                        <li className="list-group-item list-group-item-dark">Talla : {cloth.size}</li>
+                                                                        <li className="list-group-item list-group-item-dark">Moda : {cloth.fashion}</li>
+                                                                        <li className="list-group-item list-group-item-dark">Color : {cloth.color}</li>
+                                                                    </ul>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </List.Item>
+                                            )}
+                                        />
                                     </div>
                                     <div>
                                         <label>Clientes</label>
                                         <select className="custom-select" required id='client_id' name='client_id'
                                             value={this.state.client_id || ''}
                                             onChange={this.onChange.bind(this)}
-                                            required>
-                                            <option selected disabled value="">Elige...</option>
+                                            >
+                                            <option disabled  value="">Elige...</option>
                                             {this.state.clients.map((client, index) => (
                                                 <option key={index} value={client.id}>
                                                     {client.name} {client.last_name} - {client.address} - {client.cellphone}
@@ -140,7 +170,7 @@ export default class Rental extends React.Component {
                                     type="submit"
                                     className="btn btn-primary btn-block mt-4"
                                     onClick={this.onSubmit.bind(this)}>
-                                    SUBMIT
+                                    Registrar Alquiler
                         </button>
                             </form>
                         </div>
--- src/components/js/RentalFunctions.js
+++ src/components/js/RentalFunctions.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { API_CONSTANT_MAP } from './api/index'
 
+let date_now = new Date()
+
 export const getRental = () => {
     return axios.get(API_CONSTANT_MAP.rental, {
         headers: {
@@ -34,17 +36,21 @@ export const getClothWithOutRental = () => {
 }
 
 export const createRental = (date_return, price, cloth, client) => {
-    return axios.post(API_CONSTANT_MAP.rental, {
-        date_return: date_return,
-        price: price,
-        clothId: cloth,
-        clientId: client,
-    },
-        {
-            headers: { 'Content-Type': 'application/json' }
-        }
-    ).then(res => {
-        console.log(res)
-    })
-
+    let return_date = new Date(date_return)
+    if (date_now < return_date && parseInt(price) >= 5000){
+        return axios.post(API_CONSTANT_MAP.rental, {
+            date_return: date_return,
+            price: price,
+            clothId: cloth,
+            clientId: client,
+        },
+            {
+                headers: { 'Content-Type': 'application/json' }
+            }
+        ).then(res => {
+            console.log(res)
+        })
+    }else{
+        console.log("Fecha mal ingresada o precio menor a 5000")
+    }
 }
