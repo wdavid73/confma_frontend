fc8f6dbf64d66f3490c3a1c7c72e6481f8143bbe
Wilson Padilla <wpadilla4@cuc.edu.co>
2020-09-07 15:57:34 -0500

    iconos en menu principal cambiados

--- src/components/navigation/Menu.js
+++ src/components/navigation/Menu.js
@@ -9,6 +9,16 @@ import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
 } from "@ant-design/icons";
+import homepage from "../../assets/homepage.png";
+import user from "../../assets/man.png";
+import cloth from "../../assets/fashion.png";
+import rental from "../../assets/hang-clothes.png";
+import quotation from "../../assets/payment.png";
+import add_user from "../../assets/add-user.png";
+import find_user from "../../assets/find-my-friend.png";
+import add_rental from "../../assets/laundry.png";
+import list from "../../assets/checklist.png";
+import add_quo from "../../assets/boton.png";
 
 import "../../css/basic.css";
 
@@ -51,32 +61,34 @@ export default class MenuCustom extends React.Component {
           <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
             <Menu.Item key="1">
               <Link to="/">
-                <AppstoreOutlined />
-                <span> Inicio </span>
+                <img src={homepage} width="25" alt="homepage" />
+                <span> INICIO </span>
               </Link>
             </Menu.Item>
             <SubMenu
               key="sub1"
               title={
                 <span>
-                  <UserOutlined />
+                  <img src={user} width="25" alt="user" />
                   <span>Clientes</span>
                 </span>
               }
             >
               <Menu.Item key="3">
                 <Link to="/clients">
+                  <img src={add_user} width="25" alt="add_user" />
                   <span> Registro y Listado </span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="4">
                 <Link to="/clients/find">
+                  <img src={find_user} width="25" alt="find_user" />
                   <span> Buscar </span>
                 </Link>
               </Menu.Item>
             </SubMenu>
             <Menu.Item key="5">
-              <SkinOutlined />
+              <img src={cloth} width="25" alt="cloth" />
               <Link to="/cloth">
                 <span> Prendas </span>
               </Link>
@@ -86,18 +98,20 @@ export default class MenuCustom extends React.Component {
               key="sub2"
               title={
                 <span>
-                  <ProfileOutlined />
+                  <img src={rental} width="25" alt="rental" />
                   <span>Alquileres</span>
                 </span>
               }
             >
               <Menu.Item key="6">
                 <Link to="/rental">
+                  <img src={add_rental} width="25" alt="add_rental" />
                   <span> Registro de Alquileres </span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="7">
                 <Link to="/rental/list">
+                  <img src={list} width="25" alt="list_rental" />
                   <span> Listado de Alquileres </span>
                 </Link>
               </Menu.Item>
@@ -107,12 +121,14 @@ export default class MenuCustom extends React.Component {
               key="sub3"
               title={
                 <span>
-                  <ProfileOutlined /> <span>Cotizaciones</span>
+                  <img src={quotation} width="25" alt="quotation" />{" "}
+                  <span>Cotizaciones</span>
                 </span>
               }
             >
               <Menu.Item key="8">
                 <Link to="/quotation">
+                  <img src={add_quo} width="25" alt="add_quo" />
                   <span> Registar Cotizaciones </span>
                 </Link>
               </Menu.Item>
@@ -135,6 +151,12 @@ export default class MenuCustom extends React.Component {
             <div style={{ textAlign: "center" }}>{this.props.children}</div>
           </Content>
           <Footer style={{ textAlign: "center" }}>
+            <div>
+              Icons from{" "}
+              <a href="https://www.flaticon.com/" title="Flaticon">
+                www.flaticon.com
+              </a>
+            </div>
             Ant Design ©2018 Created by Ant UED
             <small className="text-muted"> - Confecciones Maribel</small>
           </Footer>
